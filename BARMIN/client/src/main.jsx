2da9aef20f7d4231a08c768df088f855d0904b53
cod3prod <cod3prod@gmail.com>
2024-09-15 20:31:31 +0900

    react-router-dom

--- BARMIN/client/src/main.jsx
+++ BARMIN/client/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import Contact from "./routes/contact"
+import ErrorPage from "./error-page"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader : rootLoader,
+    action : rootAction,
+    children : [
+      {
+        path: "contacts/:contactId",
+        element : <Contact />,
+      },
+    ],
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
