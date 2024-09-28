fbaab100ab6ab4b2ae67f6ebe3850c95c71d41bd
cod3prod <cod3prod@gmail.com>
2024-09-28 16:14:33 +0900

    🎨 add controller

---
+++ server/controllers/locations.js
@@ -0,0 +1,38 @@
+import Location from "../models/location.js";
+
+const getAll = async (req, res) => {
+  const locations = await Location.find({});
+  res.json(locations);
+};
+
+const create = async (req, res) => {
+  const location = new Location(req.body);
+  await location.save();
+  res.json({ success: true, redirect: location._id });
+};
+
+const getWithReviews = async (req, res) => {
+  const location = await Location.findById(req.params.id).populate("reviews");
+  res.json(location);
+};
+
+const update = async (req, res) => {
+  const { id } = req.params;
+  await Location.findByIdAndUpdate(id, {
+    ...req.body,
+  });
+  res.json({ success: true });
+};
+
+const remove = async (req, res) => {
+  const { id } = req.params;
+  await Location.findByIdAndDelete(id);
+  res.json({ success: true });
+};
+
+const getDetails = async (req, res) => {
+  const location = await Location.findById(req.params.id);
+  res.json(location);
+};
+
+export default { getAll, create, getWithReviews, update, remove, getDetails };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,56 +1,15 @@
 import express from "express";
 import passport from "passport";
-import wrapAsync from "../utils/wrapAsync.js";
-import User from "../models/user.js";
-import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../config/config.js";
+import wrapAsync from "../utils/wrapAsync.js";;
 import { validateUser } from "../middlewares.js";
+import users from "../controllers/users.js";
 
 const router = express.Router();
 
-router.post(
-  "/register",
-  validateUser,
-  wrapAsync(async (req, res) => {
-    {
-      try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username, role: "user" });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, (err) => {
-          if (err) return next(err);
-          const token = jwt.sign(
-            { _id: user._id, username: username, role: "user" },
-            JWT_SECRET,
-            { expiresIn: "2h" }
-          );
-          res.json({ success: true, message: "회원가입 성공", token });
-        });
-      } catch (e) {
-        res.json({ success: false, message: e.message });
-      }
-    }
-  })
-);
+router.post("/register", validateUser, wrapAsync(users.register));
 
-router.post("/login", passport.authenticate("local"), (req, res) => {
-  const user = req.user;
-  const token = jwt.sign(
-    { _id: user._id, username: user.username, role: user.role },
-    JWT_SECRET,
-    { expiresIn: "2h" }
-  );
-  res.json({ success: true, message: "로그인 성공", token });
-});
+router.post("/login", passport.authenticate("local"), users.login);
 
-router.get("/logout", (req, res) => {
-  req.logout((err)=>{
-    if(err){
-      console.log("에러", err);
-      return next(err);
-    }
-    res.json({ success: true, message: "로그아웃 성공" });
-  });
-});
+router.get("/logout", users.logout);
 
 export default router;
